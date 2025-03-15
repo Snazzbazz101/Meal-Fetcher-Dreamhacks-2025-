2976c78753098bc724c640c61f02d1f254a32116
Ayden Badyal <aydenbadyal@gmail.com>
2025-03-15 11:21:13 -0700

    Refactor page.js and page2.js
    - Import Page2 component in page.js
    - Add recipe card and ingredients section in page2.js
    - Pass ingredients as prop to recipe_1 function in page2.js

--- dreamproj/page2.js
+++ dreamproj/page2.js
@@ -1,37 +1,40 @@
 import React from 'react'
 
-const page2 = () => {
-recipe_1();
+// creates function for recipe 1 in which it returns the name of the recipe and then the recipe itself under the name
+// when touching the recipe itself it will then display the instructions on how to make it
+// the recipe will be displayed in a card format
+// will not have picture of food
+const recipe_1 = (ingredients) => {
   return (
-    <header className="header">
-      <h1>Website Logo</h1>
-      <nav>
-        <ul className="nav-list">
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
+    <div className="border border-gray-300 p-4 rounded-md">
+      {/* Recipe Card */}
+      <div className="text-center border border-white p-0.5 rounded-sm mx-auto my-1 w-fit">
+        <h2 className="text-4xl m-0">Recipe 1</h2>
+      </div>
+
+      {/* Ingredients Section */}
+      <div>
+        <h3 className="text-center text-3xl">Ingredients:</h3>
+        <ul className="list-inside text-center">
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
         </ul>
-      </nav>
-    </header>
-    
-    
+      </div>
+    </div>
+  );
+}
+
+// this function will return the name of the website and then the recipe_1 function
+const page2 = () => {
+  // connection to the api would have the ingredients be passed as a prop
+  const ingredients = ['1 cup Flour', '2 Eggs', '1 cup Milk', , '2 Eggs', '1 cup Milk', '2 Eggs', '1 cup Milk'];  // Example ingredients
+
+  return (
+    <div>
+      {recipe_1(ingredients)} {/* Pass ingredients as prop */}
+    </div>
   )
 }
 
 export default page2
-
-// creates function for recipe 1 in which it return the name of the recipe and then the recipe itself under the name
-// when touching the recipe itself it will then display the instructions on how to make it
-// the recipe will be displayed in a card format
-// will not have picture of food
-const recipe_1 = () => {
-    return (
-        <div>
-        <h1>Recipe 1</h1>
-        <p>Ingredients: </p>
-        <p>Instructions: </p>
-        </div>
-    )
-}
-
