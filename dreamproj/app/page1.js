a29e2ca0b18c8932b94ea0d51d199119b7dccb6c
Daniel <181678706+Snazzbazz101@users.noreply.github.com>
2025-03-15 10:33:43 -0700

    enter button

--- dreamproj/app/page1.js
+++ dreamproj/app/page1.js
@@ -1,10 +1,14 @@
+import { Button } from '@/components/ui/button'
 import ComboboxDemo from '@/dropmenu'
 import React from 'react'
 
 const page1 = () => {
 return (
-    <div style={{ backgroundColor: 'lightyellow', height: '100vh', margin: 0 }}>
+    <div style={{ backgroundColor: 'lightyellow', height: '100vh', margin: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <ComboboxDemo />
+        <Button className='bg-green-500'>
+            Enter!
+        </Button>
     </div>
 )
 }
