f912f0c4e5475cdab634b99bb5f4ff772913d16f
Ayden Badyal <aydenbadyal@gmail.com>
2025-03-15 12:57:12 -0700

    Refactor code to import and use Page2 component

--- dreamproj/app/page.js
+++ dreamproj/app/page.js
@@ -1,10 +1,10 @@
-import page2 from "@/page2";
+import Page2 from "@/page2"; // Correct import
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      {page2()}
+      <Page2 /> {/* Render Page2 as a component */}
     </div>
   );
 }
--- dreamproj/page2.js
+++ dreamproj/page2.js
@@ -1,24 +1,39 @@
-
 import React from 'react';
 
-// creates function for recipe 1 in which it returns the name of the recipe and then the recipe itself under the name
-// when touching the recipe itself it will then display the instructions on how to make it
-// the recipe will be displayed in a card format
-// will not have picture of food
-const recipe_1 = (ingredients) => {
+// Function for recipe 1 that returns the name of the recipe and the recipe itself
+// When hovering over the recipe, it will display the instructions on how to make it
+// The recipe will be displayed in a card format without a picture of food
+const Recipe1 = ({ ingredients }) => {
   return (
-    <div className="border border-gray-300 p-4 rounded-md">
-      {/* Recipe Card */}
-      <div className="text-center border border-white p-0.5 rounded-sm mx-auto my-1 w-fit">
-        <h2 className="text-4xl m-0">Recipe 1</h2>
+    <div className="bg-blue-100 border border-gray-300 p-6 rounded-lg shadow-lg w-fit mx-auto">
+      <div className="text-center mx-auto my-2 w-fit">
+        <h2 className="text-4xl m-1 border-b-2 border-blue-600 underline text-black">Recipe 1</h2>
+      </div>
+      {/* Ingredients Section */}
+      <div>
+        <h3 className="text-center text-3xl text-blue-800">Ingredients:</h3>
+        <ul className="list-inside text-center text-lg text-gray-700">
+          {ingredients.map((ingredient, index) => (
+            <li key={index} className="my-1">{ingredient}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Recipe2 = ({ ingredients }) => {
+  return (
+    <div className="bg-blue-100 border border-gray-300 p-6 rounded-lg shadow-lg w-fit mx-auto">
+      <div className="text-center mx-auto my-2 w-fit">
+        <h2 className="text-4xl m-1 border-b-2 border-blue-600 underline text-black">Recipe 1</h2>
       </div>
 
       {/* Ingredients Section */}
       <div>
-        <h3 className="text-center text-3xl">Ingredients:</h3>
-        <ul className="list-inside text-center">
+        <h3 className="text-center text-3xl text-blue-800">Ingredients:</h3>
+        <ul className="list-inside text-center text-lg text-gray-700">
           {ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
+            <li key={index} className="my-1">{ingredient}</li>
           ))}
         </ul>
       </div>
@@ -26,16 +41,80 @@ const recipe_1 = (ingredients) => {
   );
 };
 
-// this function will return the name of the website and then the recipe_1 function
-const page2 = () => {
-  // connection to the api would have the ingredients be passed as a prop
-  const ingredients = ['1 cup Flour', '2 Eggs', '1 cup Milk'];  // Example ingredients
-
+const Recipe3 = ({ ingredients }) => {
   return (
-    <div>
-      {recipe_1(ingredients)} {/* Pass ingredients as prop */}
+    <div className="bg-blue-100 border border-gray-300 p-6 rounded-lg shadow-lg w-fit mx-auto">
+      <div className="text-center mx-auto my-2 w-fit">
+        <h2 className="text-4xl m-1 border-b-2 border-blue-600 underline text-black">Recipe 1</h2>
+      </div>
+
+      {/* Ingredients Section */}
+      <div>
+        <h3 className="text-center text-3xl text-blue-800">Ingredients:</h3>
+        <ul className="list-inside text-center text-lg text-gray-700">
+          {ingredients.map((ingredient, index) => (
+            <li key={index} className="my-1">{ingredient}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default page2;
+const Recipe4 = ({ ingredients }) => {
+  return (
+    <div className="bg-blue-100 border border-gray-300 p-6 rounded-lg shadow-lg w-fit mx-auto">
+      <div className="text-center mx-auto my-2 w-fit">
+        <h2 className="text-4xl m-1 border-b-2 border-blue-600 underline text-black">Recipe 1</h2>
+      </div>
+
+      {/* Ingredients Section */}
+      <div>
+        <h3 className="text-center text-3xl text-blue-800">Ingredients:</h3>
+        <ul className="list-inside text-center text-lg text-gray-700">
+          {ingredients.map((ingredient, index) => (
+            <li key={index} className="my-1">{ingredient}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+const Recipe5 = ({ ingredients }) => {
+  return (
+    <div className="bg-blue-100 border border-gray-300 p-6 rounded-lg shadow-lg w-fit mx-auto">
+      <div className="text-center mx-auto my-2 w-fit">
+        <h2 className="text-4xl m-1 border-b-2 border-blue-600 underline text-black">Recipe 1</h2>
+      </div>
+
+      {/* Ingredients Section */}
+      <div>
+        <h3 className="text-center text-3xl text-blue-800">Ingredients:</h3>
+        <ul className="list-inside text-center text-lg text-gray-700">
+          {ingredients.map((ingredient, index) => (
+            <li key={index} className="my-1">{ingredient}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// This function will return the name of the website and then the Recipe1 function
+const Page2 = () => {
+  // Example ingredients
+  const ingredients = ['1 cup Flour', '2 Eggs', '1 cup Milk', '1/2 cup Sugar', '1/4 cup Butter', '1 cup Flour', '2 Eggs', '1 cup Milk', '1/2 cup Sugar', '1/4 cup Butter', '1 cup Flour', '2 Eggs', '1 cup Milk', '1/2 cup Sugar', '1/4 cup Butter'];
+
+  return (
+    <div className="min-h-screen bg-red-300 flex items-center justify-center">
+      <Recipe1 ingredients={ingredients} /> 
+      {/* <Recipe2 ingredients={ingredients} />
+      <Recipe3 ingredients={ingredients} />
+      <Recipe4 ingredients={ingredients} />
+      <Recipe5 ingredients={ingredients} /> */}
+    </div>
+    
+  );
+};
+
+export default Page2;
